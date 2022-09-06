e02c809d9c94bf36ed7c0f9451dff7d8396e38b2
WON_JUN_SON <80496821+sonwonjun103@users.noreply.github.com>
2022-09-06 17:58:34 +0900

    Add files via upload

---
+++ 자바 객체지향 프로그래밍 입문/ch01/HelloApp4.java
@@ -0,0 +1,11 @@
+public class HelloApp4 {
+    public static void main(String[] args){
+        int sum=0;
+
+        for(int i=0;i<args.length;i++){
+            sum=sum+Integer.parseInt(args[i]);
+        }
+
+        System.out.println("합 : "+ sum);
+    }
+}
